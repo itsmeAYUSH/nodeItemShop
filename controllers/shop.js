f7c22402ba88727d9be23ae648041b3d47eb4cf7
Ayush Sagar <70971035+itsmeAYUSH@users.noreply.github.com>
2023-01-31 23:21:23 +0530

    edit and delete functionality
    edit and delete functionality

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require("../models/product");
+const Cart = require("../models/cart");
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
@@ -12,7 +13,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  Product.fetchById(prodId, (product) => {
+  Product.findById(prodId, (product) => {
     // console.log(product);
     res.render("shop/product-detail", {
       product: product,
@@ -41,9 +42,12 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
-  console.log(prodId);
-  res.redirect('/cart');
-}; 
+  // console.log(prodId);
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
+};
 
 exports.getOrders = (req, res, next) => {
   res.render("shop/orders", {
